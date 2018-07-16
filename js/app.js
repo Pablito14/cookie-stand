d99e48944fb27215c625cf99fbf092d5603fd269
Pablito14 <pablo.rosales14@gmail.com>
2018-07-16 15:00:10 -0700

    Starting the getHourlyCustomers method

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var firstAndPike = {
+  minHCustomer: 23,
+  maxHCustomer: 65,
+  avgCookieSale: 6.3,
+  avgHourlyCustomer,
+  
+  // 15 times please
+  doAThing: funtion(){
+  hours:[],
+    avgHourlyCustomer = Math.getRandomInt(minHCustomer,maxHCustomer)
+  },
+
+};
