145db917b1f3c768b2bb0529aefe132966c71951
Pablito14 <pablo.rosales14@gmail.com>
2018-07-16 17:42:33 -0700

    Got genRandomHourlyCustomers to work as a function at the end of the js doc

--- js/app.js
+++ js/app.js
@@ -1,18 +1,34 @@
 'use strict';
 
+
+
 var firstAndPike = {
   minHCustomer: 23,
   maxHCustomer: 65,
   avgCookieSale: 6.3,
-  avgHourlyCustomer,
-  
-  // 15 times please
-  getHourlyCustomers: function(location){
-      var min = Math.floor(location.minHCustomer);
-      var max = Math.ciel(location.maxHCustomer);
-      return Math.floor(Math.random()) * (max - min +1) + min
+  avgHourlyCustomer: 0,
+  avgHourlyCookieSold: 0,
+  randomHourlyCustomers: 0,
+  amountOfCookiesSoldPH: [],
+
+  getHourlyCookiesSold: function(){
+    for(var i = 0; i < 14; i++){
+      this.avgHourlyCookieSold = this.avgHourlyCustomer * this.avgCookieSale;
+      this.avgHourlyCookieSold.push(this.amountOfCookiesSoldPH);
+      i++;
+    }
   }
-  
-  },
+
 
 };
+var genRandomHourlyCustomers = function(location){
+  var randomHourlyCustomers;
+  var min = Math.floor(location.minHCustomer);
+  var max = Math.floor(location.maxHCustomer + 1);
+  randomHourlyCustomers = Math.floor((Math.random()) * (max - min +1) + min);
+  console.log(randomHourlyCustomers);
+
+  return randomHourlyCustomers;
+};
+
+genRandomHourlyCustomers(firstAndPike);
