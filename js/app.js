fd45644eb353948782535ad21754d15fdc853c99
Pablito14 <pablo.rosales14@gmail.com>
2018-07-18 13:05:00 -0700

    inittializing new branch for wednesday

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var firstAndPike = {
   minHourlyCustomer: 23,
   maxHourlyCustomer: 65,
   avgCookieSalePerCustomer: 6.3,
-  hours: [],
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'],
   customersPerHour: [],
   cookiesPerHour: [],
   dailyTotal: 0,
