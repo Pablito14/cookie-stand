4c5c34082b564801131920c8d730811175a6f33e
Pablito14 <pablo.rosales14@gmail.com>
2018-07-19 15:58:55 -0700

    Still disectin and re-naming for clarity

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function genRandomHourlyCustomers(min, max){
+function genOneRandomCustomer(min, max){
   return Math.random() * (max - min) + min;
 }
 // hours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '] Perhaps better used as a global variable?
@@ -12,29 +12,28 @@ var store = {
   maxHourlyCustomer: 65,
   avgCookieSalePerCustomer: 6.3,
   hours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '],
-  customersPerHour: [],
-  cookiesPerHour: [],
+  randomHourlyCustomers: [], // will this array be populated recursively by the random num gen method?
+  randomHourlyCookiesArray: [], // same question as above
   dailyTotal: 0,
 
   // methods
-  calculateCustomers: function(){
-
+  generateCustomers: function(){ // make the array of randomized customers
     for(var hour of store.hours){
-      var rando = genRandomHourlyCustomers(store.minHourlyCustomer, store.maxHourlyCustomer);
-      store.customersPerHour.push(rando);
+      var rando = genOneRandomCustomer(store.minHourlyCustomer, store.maxHourlyCustomer); // rando is one random number(customer)
+      store.randomHourlyCustomers.push(rando); // push it into the array
     }
-    return store.customersPerHour;
+    return store.randomHourlyCustomers; // return the array
   },
 
   calculateSales: function(){
-    store.calculateCustomers();
+    store.generateCustomers();
 
-    for (var numCustomers of store.customersPerHour){
-      var cookies = Math.ceil(store.avgCookieSalePerCustomer * numCustomers);
-      store.cookiesPerHour.push(cookies);
+    for (var numOfCustomersInHour of store.randomHourlyCustomers){ // for every element in the array [randomHourlyCustomers]
+      var cookies = Math.ceil(store.avgCookieSalePerCustomer * numOfCustomersInHour); // cookies will be assigned the value of the store avgSale*[genOneRandomCustomer] (the average rate * the randomized number array)
+      store.randomHourlyCookiesArray.push(cookies); // push the amount of cookies purchased into the array
       store.dailyTotal += cookies;
     }
-    return store.cookiesPerHour;
+    return store.randomHourlyCookiesArray;
   },
 
   render: function(){
@@ -47,7 +46,7 @@ var store = {
 
     for (var idx in store.hours) {
       var liEl = document.createElement('li');
-      liEl.textContent = store.hours[idx] + ': ' + store.cookiesPerHour[idx] + ' cookies';
+      liEl.textContent = store.hours[idx] + ': ' + store.randomHourlyCookiesArray[idx] + ' cookies';
       ulEl.appendChild(liEl);
     }
 
