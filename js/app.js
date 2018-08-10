66773e1bacc994753cd1880ec5d4853b489a7f51
Pablito14 <pablo.rosales14@gmail.com>
2018-08-09 17:27:47 -0700

    Cleaned up and tried to move forward

--- js/app.js
+++ js/app.js
@@ -1,27 +1,17 @@
 'use strict';
 
-var hours = [
-  '6am ',
-  '7am ',
-  '8am ',
-  '9am ',
-  '10am ',
-  '11am ',
-  '12pm ',
-  '1pm ',
-  '2pm ',
-  '3pm ',
-  '4pm ',
-  '5pm ',
-  '6pm ',
-  '7pm ',
-  '8pm '
-];
-var stores = [];
+/****************************************************************************/
+/****************************Global Variables********************************/
+/****************************************************************************/
+
+var hours = ['6am ','7am ','8am ','9am ','10am ','11am ','12pm ','1pm ','2pm ','3pm ','4pm ','5pm ','6pm ','7pm ','8pm '];
+var allStores = [];
 var dailyTotalsArr = [];
 var table = document.createElement('table');
 
-/*************************************************************/
+/****************************************************************************/
+/***************************Store Constructor********************************/
+/****************************************************************************/
 function Store(name, minCustomer, maxCustomer, average) {
   this.name = name;
   this.minCustomer = minCustomer;
@@ -29,10 +19,13 @@ function Store(name, minCustomer, maxCustomer, average) {
   this.avgCookieSalePerCustomer = average;
   this.dailyTotal = 0;
   this.cookieSales = [];
-  stores.push(this);
   this.renderData();
+  allStores.push(this);
+
 }
-/*************************************************************/
+/****************************************************************************/
+/*******************Data Related ƒ(x) & prototypes***************************/
+/****************************************************************************/
 Store.prototype.generateCustomers = function(min, max) {
   return Math.random() * (max - min) + min;
 };
@@ -55,8 +48,6 @@ Store.prototype.dailyTotals = function() {
   return this.dailyTotal;
 };
 
-/************************************************************* */
-
 function calculateAllTotals(){
   var totalT = 0;
   for (var i = 0; i < dailyTotalsArr.length; i++) {
@@ -65,21 +56,25 @@ function calculateAllTotals(){
   return totalT;
 }
 
+/****************************************************************************/
+/**************************Making the Table**********************************/
+/****************************************************************************/
+
 function makeHeaderRow() {
   // creating a table header element
   var tableHeaderElement = document.createElement('thead');
   // creating a first row with hours
   var trTableHeaderElement = document.createElement('tr');
   // put an empty cell at A1
-  trTableHeaderElement.appendChild(document.createElement('td'));
+  trTableHeaderElement.appendChild(document.createElement('th'));
   // run throught the array of hours and create a cell for each element
   for (var hour of hours) {
-    var tdHeaderElement = document.createElement('td');
+    var tdHeaderElement = document.createElement('th');
     tdHeaderElement.appendChild(document.createTextNode(hour));
     trTableHeaderElement.appendChild(tdHeaderElement);
   }
   // make the last cell, the total cell
-  var tdFirstTotalElement = document.createElement('td');
+  var tdFirstTotalElement = document.createElement('th');
   tdFirstTotalElement.appendChild(
     document.createTextNode('Daily location Total')
   );
@@ -88,31 +83,29 @@ function makeHeaderRow() {
   table.appendChild(tableHeaderElement);
 }
 
-/*************************************************************/
 function makeFooterRow()
 {
-  var trEl = document.createElement('tr');
-  var emptyTD = document.createElement('td');
-  emptyTD.textContent = 'Hourly Totals:';
-  trEl.appendChild(emptyTD);
+  var tFootEl = document.createElement('tfoot');
+  var totalLabelEl = document.createElement('th');
+  totalLabelEl.textContent = 'Hourly Totals:';
+  tFootEl.appendChild(totalLabelEl);
   for(var i = 0; i < hours.length; i++)
   {
     var hourTotal = 0;
-    for(var j = 0; j < stores.length; j++){
-      hourTotal += stores[j].cookieSales[i];
+    for(var j = 0; j < allStores.length; j++){
+      hourTotal += allStores[j].cookieSales[i];
     }
     var tdEl = document.createElement('td');
     tdEl.textContent = hourTotal;
-    trEl.appendChild(tdEl);
+    tFootEl.appendChild(tdEl);
   }
   var tdEl2 = document.createElement('td');
   tdEl2.textContent = calculateAllTotals();
-  trEl.appendChild(tdEl2);
-  table.appendChild(trEl);
+  tFootEl.appendChild(tdEl2);
+  table.appendChild(tFootEl);
 }
-/*************************************************************/
 
-var createTable = function() {
+var createTableArea = function() {
   makeHeaderRow();
   var grabTable = document.getElementById('Table-goes-heresy');
   grabTable.appendChild(table);
@@ -121,7 +114,7 @@ var createTable = function() {
 Store.prototype.renderData = function() {
   this.calculateSales();
   var trEl = document.createElement('tr');
-  var tdNameEl = document.createElement('td');
+  var tdNameEl = document.createElement('th');
   tdNameEl.textContent = this.name;
   trEl.appendChild(tdNameEl);
   for (var i = 0; i < hours.length; i++) {
@@ -134,58 +127,26 @@ Store.prototype.renderData = function() {
   trEl.appendChild(tdEl2);
   table.appendChild(trEl);
 };
-/*************************************************************/
-
-// Store.prototype.render = function(){
-//   this.calculateSales();
-//   var trEl = document.createElement('tr');
-//   var grabTable = document.getElementById('Table-goes-heresy').firstElementChild;
-//   grabTable.appendChild(trEl);
-//   var storeName = document.createElement('td');
-//   storeName.textContent = this.name;
-//   trEl.appendChild(storeName);
-//   for(var i = 0; i < hours.length; i++){
-//     var class1 = 'green';
-//     var class2 = 'pink';
-//     var classToChange;
-//     if(i % 2 === 0){
-//       classToChange = class1;
-//     } else {
-//       classToChange = class2;
-//     }
-//     var tdEl = document.createElement('td');
-//     tdEl.className = classToChange;
-//     console.log('random thing', tdEl);
-//     tdEl.textContent = this.randomHourlyCookiesArray[i];
-//     trEl.appendChild(tdEl);
-//   }
-//   var totalEl = document.createElement('td');
-//   totalEl.textContent = this.dailyTotal;
-//   trEl.appendChild(tdEl);
-// };
-
-// var nameEl = document.getElementById('name');
-// var minEl = document.getElementById('min');
-// var maxEl = document.getElementById('max');
-// var avgEl = document.getElementById('avg');
 
 var formEl = document.getElementById('formMain');
 formEl.addEventListener('submit', function(event) {
   event.preventDefault();
-  console.log('event.target', event);
-  console.log(event.target.storename.value);
   var storename = event.target.name.value;
   var minC = event.target.minC.value;
   var maxC = event.target.maxC.value;
   var avgS = event.target.avgC.value;
-  var submitStore = new Store(storename, minC, maxC, avgS, [], [], 0);
-  for (var i = 0; i < hours.length; i++) {
-    submitStore.render();
-  }
+
+  // for (var i = 0; i < hours.length; i++)
+  var submitStore = new Store(storename, minC, maxC, avgS);
+  submitStore.renderData();
+
 });
 
-createTable();
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
 
+createTableArea();
 
 new Store('First and Pike', 23, 65, 6.3);
 new Store('SeaTac Airport', 3, 24, 1.2);
